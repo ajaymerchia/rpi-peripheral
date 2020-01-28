1700b3ca7001ebc8e880052feebd31052a52c7ea
Azaad <ucb.azaad@gmail.com>
2020-01-28 15:20:27 -0800

    updated start script

--- robinslight_start.sh
+++ robinslight_start.sh
@@ -1,6 +1,6 @@
-cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
+icd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
 cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
 cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
-cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 
-# cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start
+# cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 
+ cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start
 

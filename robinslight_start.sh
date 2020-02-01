d71fcb2b040e7f5787466f78bf5952060ddbd63f
Abhinav Pottabathula <apottabathula@berkeley.edu>
2020-01-31 18:30:49 -0800

    updated rlight start script

--- robinslight_start.sh
+++ robinslight_start.sh
@@ -1,6 +1,5 @@
-icd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
+cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
 cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
 cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
 # cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 
- cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start
-
+cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start

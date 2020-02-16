d9a41542a44895609ccf8e8f20a9423dd7c8af16
Shreyas Patankar <sgpatankar@berkeley.edu>
2020-02-15 16:53:13 -0800

    white static BB

--- robinslight_start.sh
+++ robinslight_start.sh
@@ -1,4 +1,5 @@
-cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
+cd /home/pi/wkspc/rpi-peripheral && sudo ./status white 45 &
+# cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
 cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
 cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
 # cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 

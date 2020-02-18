16033b672fdc599c76f8db496c32de019a066a9d
Azaad <ucbazaad@gmail.com>
2020-02-18 08:41:28 -0800

    Reset Boot Script

--- robinslight_start.sh
+++ robinslight_start.sh
@@ -1,6 +1,9 @@
-cd /home/pi/wkspc/rpi-peripheral && sudo ./status white 45 &
-# cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
-# cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
-# cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
-# cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 
-# cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start
+# STATIC LIGHT BACKUP
+# cd /home/pi/wkspc/rpi-peripheral && sudo ./status white 45 &
+
+# ROBINOS
+ cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
+ cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
+ cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
+ cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 
+ cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start

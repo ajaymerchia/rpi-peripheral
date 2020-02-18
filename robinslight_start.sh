276f7676610f7eb337a9cfbbc363d9676c0fa339
Azaad <ucbazaad@gmail.com>
2020-02-18 08:46:44 -0800

    untracked bs, fixed startup script

--- robinslight_start.sh
+++ robinslight_start.sh
@@ -2,8 +2,10 @@
 # cd /home/pi/wkspc/rpi-peripheral && sudo ./status white 45 &
 
 # ROBINOS
- cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
- cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
- cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
- cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 
- cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start
+cd /home/pi/wkspc/rpi-peripheral && sudo ./status red &
+cd /home/pi/wkspc/rpi-peripheral && sudo ./bleconfig
+cp /home/pi/wkspc/rpi-peripheral/server/bleno_test.js /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno/test.js
+cd /home/pi/wkspc/rpi-peripheral/server && sudo npm start
+
+# Advertising Config
+# cd /home/pi/wkspc/rpi-peripheral/server/node_modules/bleno && sudo node test.js 

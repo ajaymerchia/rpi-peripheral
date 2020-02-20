7f610022cd95a3538cc95f9c9396cec6d3891ce1
Ajay Merchia <ajaymerchia@berkeley.edu>
2020-02-20 07:04:41 -0800

    Connect to Network Protocol

--- server/services/network/characteristics/connect.js
+++ server/services/network/characteristics/connect.js
@@ -3,6 +3,10 @@ var util = require('util');
 const Constants = require('../../../constants')
 const io = require('../../../io')
 const dispatcher = require('../../../dispatcher')
+const fs = require('fs')
+
+const child = require('child_process');
+const spawn = child.spawn
 
 var BlenoCharacteristic = bleno.Characteristic;
 
@@ -20,6 +24,19 @@ util.inherits(NetworkConnectCharacteristic, BlenoCharacteristic);
 
 NetworkConnectCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
   var commaSeparatedESSIDandPSK = data.toString()
+  console.log(commaSeparatedESSIDandPSK);
+
+  commaSeparatedESSIDandPSK = commaSeparatedESSIDandPSK.split(",")
+  var network = commaSeparatedESSIDandPSK[0]
+  var pass = commaSeparatedESSIDandPSK[1]
+
+  var contentToAddToWPAFile = `network={\n\tssid=${network}\n\tpsk=${pass}}`
+  console.log("adding network config to wpa_supplicant");
+  console.log(contentToAddToWPAFile);
+  fs.appendFileSync('/etc/wpa_supplicant/wpa_supplicant.conf', contentToAddToWPAFile);
+  spawn("wpa_cli", ["-i", "wlan0", "reconfigure"]).on('close', (exit) => {
+    spawn("ifconfig", ["wlan0", "up"])
+  })
 
   callback(this.RESULT_SUCCESS)
 };

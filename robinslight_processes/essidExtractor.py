66c1bfbf1fa4db0ed6c31c40ef7f120e97f5c1d3
Shreyas Patankar <sgpatankar@berkeley.edu>
2020-02-20 06:55:01 -0800

    essid fixes

--- robinslight_processes/essidExtractor.py
+++ robinslight_processes/essidExtractor.py
@@ -1,7 +1,7 @@
 import os
 
 def extractESSIDsAsList():
-	buff = os.popen('sudo iwlist wlan0 scan').read().split('ESSID:"')
+	stream = os.popen('sudo iwlist wlan0 scan').read().split('ESSID:"')
 	unfilteredNames = [s.split('\n')[0][:-1] for s in stream][1:]
 	return [n for n in unfilteredNames if len(n) > 0]
 

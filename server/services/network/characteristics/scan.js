3936fb6572ac4c3627c5b72b444c496233a0b949
Shreyas Patankar <sgpatankar@berkeley.edu>
2020-02-20 06:38:38 -0800

    adding scan service

--- server/services/network/characteristics/scan.js
+++ server/services/network/characteristics/scan.js
@@ -4,6 +4,9 @@ const Constants = require('../../../constants')
 const io = require('../../../io')
 const dispatcher = require('../../../dispatcher')
 
+const child = require('child_process');
+const spawn = child.spawn
+
 var BlenoCharacteristic = bleno.Characteristic;
 
 var NetworkScanCharacteristic = function() {
@@ -20,12 +23,19 @@ util.inherits(NetworkScanCharacteristic, BlenoCharacteristic);
 
 NetworkScanCharacteristic.prototype.onReadRequest = function(offset, callback) {
 
-  var commaSeparatedNetworkList = null;
+  var commaSeparatedNetworkList = "";
+  const fetch = spawn("python", ['/home/pi/wkspc/rpi-peripheral/robinslight_processes/essidExtractor.py'])
+  fetch.on('data', (chunk) => {
+  	  console.log("recieved chunk:", chunk)
+      commaSeparatedNetworkList += chunk
+  })
+  fetch.on('close', (exit_code) => {
+  	  console.log("writing buffer")
+  	  console.log(commaSeparatedNetworkList)
+	  this._value = new Buffer(commaSeparatedNetworkList)
+	  callback(this.RESULT_SUCCESS, this._value.slice(offset, this._value.length));
+  })
 
-  // set commaSeparatedNetworkList
-
-  this._value = new Buffer(commaSeparatedNetworkList)
-  callback(this.RESULT_SUCCESS, this._value.slice(offset, this._value.length));
 };
 
 

b5763b10b9f0f7151ced1d476fce0d6e9b32302c
Shreyas Patankar <sgpatankar@berkeley.edu>
2020-02-20 06:42:31 -0800

    adding essidExtractor

---
+++ robinslight_processes/essidExtractor.py
@@ -0,0 +1,14 @@
+import os
+
+def extractESSIDsAsList():
+	buff = os.popen('sudo iwlist wlan0 scan').read().split('ESSID:"')
+	unfilteredNames = [s.split('\n')[0][:-1] for s in stream][1:]
+	return [n for n in unfilteredNames if len(n) > 0]
+
+def extractFormattedESSIDs():
+	soln = ''
+	for net in extractESSIDsAsList():
+		soln += (net + ',')
+	return soln
+
+print(extractFormattedESSIDs())

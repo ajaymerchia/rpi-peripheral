2ba814c3a47056fb2a1a3a6d2adcec6cddf15112
Ajay Merchia <ajaymerchia@berkeley.edu>
2020-02-20 19:42:23 -0800

    More efficient lightshow runtime between effects.

--- robinslight_processes/lightshow.py
+++ robinslight_processes/lightshow.py
@@ -13,12 +13,11 @@ if __name__ == '__main__':
     r = open(targetFile, "rb+")
     blobLoaded = r.read().strip()
     lightshow = pickle.loads(blobLoaded)
-    print(lightshow[-5:])
 
     transmissionLag = 0
     if sent:
         transmissionLag = time.time() - float(sys.argv[2])
-        print("took {0}s to transmit".format(transmissionLag))
+        # print("took {0}s to transmit".format(transmissionLag))
 
     offset = 0
     startIdx = 0
@@ -29,19 +28,23 @@ if __name__ == '__main__':
     # rollback the previous step
     startIdx -= 1
     offset -= lightshow[startIdx][1]
-    print("{0}s delay skips {1} instructions".format(transmissionLag, startIdx))
+    # print("{0}s delay skips {1} instructions".format(transmissionLag, startIdx))
 
     # Usual duration - (totalLag - compensated lag)
     firstDuration = lightshow[startIdx][1] - (transmissionLag - offset)
 
-    print("starting at instruction {0} with {1} left for that instruction".format(startIdx, firstDuration))
+    lightshow[startIdx] = (lightshow[startIdx][0], firstDuration)
+
+    # print("starting at instruction {0} with {1} left for that instruction".format(startIdx, firstDuration))
 
     for i in range(startIdx, len(lightshow)):
         pair = lightshow[i]
-        # if offset < transmissionLag:
-        #     continue
+        setTime = time.time()
         ledAPI.setStripColor(pair[0])
-        ledAPI.wait(pair[1] if i != startIdx else firstDuration)
+        executionTime = time.time() - setTime
+        
+        time.sleep(max(pair[1] - executionTime, 0))
+
 
     print("SHOW COMPLETE")
     ledAPI.setStripColor((0,0,0))

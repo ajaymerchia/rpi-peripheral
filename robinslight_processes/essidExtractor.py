f29d150a0578ac95e1993b7d5a407aa94d148c00
Ajay Merchia <ajaymerchia@berkeley.edu>
2020-02-22 00:59:18 -0800

    Removed duplicates from network list

--- robinslight_processes/essidExtractor.py
+++ robinslight_processes/essidExtractor.py
@@ -3,7 +3,8 @@ import os
 def extractESSIDsAsList():
 	stream = os.popen('sudo iwlist wlan0 scan').read().split('ESSID:"')
 	unfilteredNames = [s.split('\n')[0][:-1] for s in stream][1:]
-	return [n for n in unfilteredNames if len(n) > 0]
+	duplicatesNameList = [n for n in unfilteredNames if len(n) > 0]
+	return list(dict.fromkeys(duplicatesNameList))
 
 def extractFormattedESSIDs():
 	soln = ''
